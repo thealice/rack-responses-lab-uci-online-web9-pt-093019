d7dabed73cc8be2a944d07f9a0a61da153e16060
Alice Freda <alicefreda@gmail.com>
2020-01-07 21:35:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     time = Time.now
-  when time < 120000 resp.write "Good Morning!"
+  when time < 12 resp.write "Good Morning!"
   else resp.write "Good Afternoon!"
   end
 end
