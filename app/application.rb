17963b8d62813c3064d0a929aa1656cd937a366f
Alice Freda <alicefreda@gmail.com>
2020-01-07 21:50:34 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,10 +3,10 @@ class Application
     resp = Rack::Response.new
     time = Time.now.to_a
 
-    if time[3] < 12
-      resp.write "Good Morning!"
-    else
-      resp.write "Good Afternoon!"
-    end
+    # if time[3] < 12
+    #   resp.write "Good Morning!"
+    # else
+    #   resp.write "Good Afternoon!"
+    # end
   end
 end
